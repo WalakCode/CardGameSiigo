005eb35dc5aa5730349c54fffd9e751bd1638152
Centro CPYA La Dorada <62_Jornada1@DRDDFPRCLFSD007.FormacionSena.RED>
2023-09-06 13:51:43 -0500

    se elimino lo que contenia el archivio js de js

--- public/js/scripts.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('popstate', (event) => {
-    const state = event.state;
-    if (state) {
-      // Puedes utilizar el estado para tomar decisiones basadas en la entrada del historial
-      
-      console.log('Se ha navegado hacia atrás o adelante en el historial:', state);
-    }
-  });
-  
